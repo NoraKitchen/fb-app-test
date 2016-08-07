0d1f12ef23bedfd74c53481b70d8bd425f7e01bc
NoraKitchen <narwhaltortellini@gmail.com>
2016-08-07 16:42:35 -0600

    integrated with wit. not getting back resp on fb

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var server = express();
 var port = process.env.PORT || 8080; //process is note obj, looks if user has port? was in heroku notes
 var bodyParser = require('body-parser');
-var  request = require('request');
+var request = require('request');
 
 server.use(bodyParser.json());
 
@@ -14,10 +14,79 @@ server.listen(port, function () { console.log("server running on port " + port) 
 // })
 //dirname says use current directory path
 
-server.use('/test', express.static(__dirname + '/public'))
+// server.use('/test', express.static(__dirname + '/public'))
+
+//*
+var Wit = null;
+let log = null;
+try {
+    // if running from repo
+    Wit = require('../').Wit;
+} catch (e) {
+    Wit = require('node-wit').Wit;
+}
+
+// var WIT_TOKEN = process.env.WIT_TOKEN;
+var WIT_TOKEN = "KY2YTSDBZKPRRVG5TF4GKHFGPXJ2WP2G";
+
+
+// This will contain all user sessions.
+// Each session has an entry:
+// sessionId -> {fbid: facebookUserId, context: sessionState}
+var sessions = {};
+
+function findOrCreateSession(fbid) {
+    var sessionId;
+    // Let's see if we already have a session for the user fbid
+    Object.keys(sessions).forEach(function (key) {
+        if (sessions[key].fbid === fbid) {
+            // Yep, got it!
+            sessionId = key;
+        }
+    });
+    if (!sessionId) {
+        // No session found for user fbid, let's create a new one
+        sessionId = new Date().toISOString();
+        sessions[sessionId] = { fbid: fbid, context: {} };
+    }
+    return sessionId;
+};
+
+var actions = {
+    send(request, response) {
+        //in fb exaple had diff (args), think will work this way...
+
+        // const {sessionId, context, entities} = request;
+        // const {text, quickreplies} = response;
+
+        //retrieve user whose session belongs to
+        var recipientId = sessions[request.sessionId].fbid;
+        if (recipientId) {
+            return new Promise(function (resolve, reject) {
+                console.log('user said...', request.text);
+                console.log('sending...', JSON.stringify(response));
+                return resolve();
+            });
+        }
+    },
+    echoLocation({context, entities}) {
+        context.location = entities.location;
+        return Promise.resolve(context);
+    },
+    longTime({context, entities}) {
+        context.years = Math.random() * (100 - 2) + 2;
+        return Promise.resolve(context);
+    },
+};
+
+
+//*
+
+
 
 var VALIDATION_TOKEN = "verifyMe";
-var PAGE_ACCESS_TOKEN = "EAAMVFy1iwHkBAHzZCPDi4d3PsmxJGqFImF5onKk7iPHAsdswU56elnAhewsajEDRh6FWiBfM6pYZAp7vriKe7WVM61a5DS2pfIyR6nvzHuFojzFyC10RlB1jFgjHp3ZA42oZCLpR8twJkzcm3s6H2CPwc9IXX3NiBtZBFZBgJ3wwZDZD";
+var PAGE_ACCESS_TOKEN = "EAAMVFy1iwHkBAKtF4gZBezhyeZAdHZCgUdfwwBPwiKkGG3bTQ7cgY9JN7wZAPqfie7VEGs5tURss4qnfDhTzDXcCD7Wve3BlyZBubOYXidORWq0bQEIJpv2ZAlH8gf9JOqMe1WGh7gynBaYZB03AkCrkSUmYCvNo031ODYrmp0nHQZDZD";
+// var PAGE_ACCESS_TOKEN = process.env.WIT_TOKEN;
 
 server.get('/webhook', function (req, res) {
     if (req.query['hub.mode'] === 'subscribe' &&
@@ -51,18 +120,37 @@ server.post('/webhook', function (req, res) {
                 } else if (messagingEvent.message) {
                     //   receivedMessage(messagingEvent);
                     console.log("got message");
-                    console.log(messagingEvent.message.text);
-                    console.log(messagingEvent.sender.id);
+                    var sender = messagingEvent.sender.id;
 
-                    var messageData = {
-                        recipient: {
-                            id: messagingEvent.sender.id
-                        },
-                        message: {
-                            text: messagingEvent.message.text
-                        }
+                    //*
+                    //got a message from user, so figure out if have message history with user
+                    var sessionId = findOrCreateSession(sender);
+                    var senderText = messagingEvent.message.text;
+                    //retrieve message content
+
+                    //message could be message.text or message.attachment..
+                    if (senderText) {
+                        //forward to wit.ai bot engine
+                        //bot will run all actions till nothing left to do 
+                        wit.runActions(sessionId, senderText, sessions[sessionId].context
+                        ).then(function (context) {
+                            sessions[sessionId].context = context;
+                            //now bot is waiting for futher emssages?
+                            //based on session state/business logic, might delete session here
+                            //if (context['done']){delete sessions[sessionId]}
+                        })
                     }
-                    callSendApi(messageData);
+
+                    // //code from when message was sent by fb bot?
+                    // var messageData = {
+                    //     recipient: {
+                    //         id: messagingEvent.sender.id
+                    //     },
+                    //     message: {
+                    //         text: messagingEvent.message.text + ", or so they say."
+                    //     }
+                    // }
+                    // callSendApi(messageData);
 
                 } else if (messagingEvent.delivery) {
                     //   receivedDeliveryConfirmation(messagingEvent);
@@ -85,25 +173,27 @@ server.post('/webhook', function (req, res) {
 });
 
 
+//from when fb bot sent messages back, not relevant anymore?
+// function callSendApi(messageData) {
+//     request({
+//         uri: 'https://graph.facebook.com/v2.6/me/messages',
+//         qs: { access_token: PAGE_ACCESS_TOKEN },
+//         method: 'POST',
+//         json: messageData
 
-function callSendApi(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: messageData
+//     }, function (error, response, body) {
+//         if (!error && response.statusCode == 200) {
+//             var recipientId = body.recipient_id;
+//             var messageId = body.message_id;
 
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
+//             console.log("Successfully sent generic message with id %s to recipient %s",
+//                 messageId, recipientId);
+//         } else {
+//             console.error("Unable to send message.");
+//             console.error(response);
+//             console.error(error);
+//         }
+//     });
+// }
 
-      console.log("Successfully sent generic message with id %s to recipient %s", 
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
-    }
-  });  
-}
+var wit= new Wit({accessToken: WIT_TOKEN, actions: actions});
